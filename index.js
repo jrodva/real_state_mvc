8c0cfff7a1065fe88de78a35da1deea3def911ce
jrodva <jr.rodriguez@crowdfarming.com>
2024-01-11 16:41:44 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const PORT = 3000
+
+const express = require('express')
+const app = express()
+
+app.get('/', function(req, res) {
+  res.send('Hello world from express')
+})
+
+app.get('/us', function(req, res) {
+  res.send('We are')
+})
+
+app.get('/json', function(req, res) {
+  res.send({ msg: 'Hello world from express' })
+})
+
+app.get('/template', function(req, res) {
+  res.render('<p>Hello world from express</p>')
+})
+
+app.listen(PORT, () => {
+  console.log('Running app');
+})
